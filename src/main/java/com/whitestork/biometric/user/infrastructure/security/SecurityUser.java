57da8ed50b2b1d3c9d1f04cd601a5241082d83c2
stanislav <8268363@gmail.com>
2026-03-09 11:49:44 +0300

    update README.md

--- src/main/java/com/whitestork/biometric/user/infrastructure/security/SecurityUser.java
+++ src/main/java/com/whitestork/biometric/user/infrastructure/security/SecurityUser.java
@@ -28,4 +28,24 @@ public record SecurityUser(
   public Collection<? extends GrantedAuthority> getAuthorities() {
     return List.of();
   }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
